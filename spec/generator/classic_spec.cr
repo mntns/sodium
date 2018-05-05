f26cda13f15a90a9cc5988366ef8ba9b08329df1
Eddy Shure <github@monoton.space>
2018-05-05 12:21:56 +0200

    Add more classic generators

--- spec/generator/classic_spec.cr
+++ spec/generator/classic_spec.cr
@@ -1,8 +1,35 @@
 require "../spec_helper"
 
 describe Sodium::Generator::Classic do
-  it "generate complete graph" do
-    gen = Sodium::Generator::Classic(Int32).new()
-    g = gen.complete_graph(200)
+  gen = Sodium::Generator::Classic(Int32).new()
+  n = 10
+  
+  it "generates complete graph" do
+    g = gen.complete_graph(n)
+    edges = (0...n).to_a.combinations(2).map{|e| {e[0], e[1]}}
+    g.edges.should eq(edges)
   end
+
+  it "generates empty graph with default values" do
+    g = gen.empty_graph()
+    g.nodes.should eq([] of Int32)
+  end
+
+  it "generates empty graph with 10 nodes" do
+    g = gen.empty_graph(n)
+    g.nodes.should eq((0...n).to_a)
+  end
+
+  it "generates path graph" do
+    g = gen.path_graph(n)
+    edges = (0...n).each_cons(2).map{|e| {e[0], e[1]}}.to_a
+    g.edges.should eq(edges)
+  end
+
+  it "generates star graph" do
+    g = gen.star_graph(n)
+    edges = (1...n).map{|i| {0, i}}
+    g.edges.should eq(edges)
+  end
+
 end
--- src/sodium/generator/classic.cr
+++ src/sodium/generator/classic.cr
@@ -3,11 +3,10 @@ require "../graph"
 module Sodium
   module Generator
      class Classic(T)
-
       # Returns complete graph
       def complete_graph(n) 
         g = empty_graph(n)
-        edges = (0..n).to_a.combinations(2).map{|e| {e[0], e[1]}}
+        edges = (0...n).to_a.combinations(2).map{|e| {e[0], e[1]}}
         g.add_edges_from(edges)
         g
       end
@@ -15,10 +14,29 @@ module Sodium
       # Returns an empty graph
       def empty_graph(n = 0)
         g = Sodium::Graph(T).new()
-        g.add_nodes_from(0..n)
+        if (n != 0)
+          g.add_nodes_from(0...n)
+        end
         g
       end
 
+      # Returns the path graph
+      def path_graph(n)
+        g = empty_graph()
+        edges = (0...n).each_cons(2) do |con|
+          g.add_edge(con[0], con[1])
+        end
+        g
+      end
+
+      # Returns the star graph
+      def star_graph(n)
+        g = empty_graph()
+        g.add_node(0)
+        edges = (1...n).map {|i| {0, i}}.to_a
+        g.add_edges_from(edges)
+        g
+      end
     end
   end
 end
